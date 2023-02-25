3901475c2388782f03ad951861d8b7da8d77ad24
Wolfgang_CZ <kralicek.roman@gmail.com>
2023-02-26 00:41:59 +0100

    ordinary day

--- main.cpp
+++ main.cpp
@@ -4,25 +4,30 @@
 #include <cstring>
 #include "raylib.h"
 
+
 int main ()
 {
-    const int w_width {600};
-    const int w_height {600};
+
+    const int screen_width {800};
+    const int screen_height {800};
 
     const int upper_bound {0};
-    const int lower_bound {w_height};
+    const int lower_bound {screen_height};
     const int left_bound {0};
-    const int right_bound {w_width};
-    InitWindow(w_height,w_width, "New window");
+    const int right_bound {screen_width};
+    InitWindow(screen_height,screen_width, "New window");
 
     double const gravity {1};
     double const pi {3.1415};
 
+    double const max_zoom {5};
+    double const min_zoom {0.1};
+
     double bounciness_coef {0.8};
     double ground_bounciness_coef {0.2};
 
-    double circle_x {w_width/2};
-    double circle_y {w_height/2};
+    double circle_x {screen_width/2};
+    double circle_y {screen_height/2};
 
     double circle_radius {50};
 
@@ -33,6 +38,9 @@ int main ()
     double move_force {100};
     double jump_force {15000};
 
+
+
+
     double x_force {0};
     double y_force {0};
 
@@ -42,17 +50,36 @@ int main ()
     double accl_x {0};
     double accl_y {0};
 
+//setting up camera
+    Vector2 camera_position {screen_width/2,screen_height/2};
+
+    Camera2D camera {0};
+    camera.target = camera_position;
+    camera.offset = (Vector2){screen_width/2,screen_height/2};
+    camera.rotation = 0;
+    camera.zoom = 1;
+
     SetTargetFPS(60);
     while(WindowShouldClose()==false)
     {
-        
+                // Camera zoom controls
+
         BeginDrawing();
         ClearBackground(WHITE);
+        BeginMode2D(camera);
+        
+        camera.target = camera_position;
         //game logic
         friction = 0.02;
         bool grounded {false};
         if((lower_bound-(circle_y+circle_radius))<=0) grounded = true;
+        DrawRectangle(0, 0, screen_width, screen_height, GREEN);
         DrawCircle(circle_x,circle_y,circle_radius,BLUE);
+
+
+        camera.zoom += ((float)GetMouseWheelMove()*0.2f);
+        if(camera.zoom > max_zoom) camera.zoom = max_zoom;
+        if(camera.zoom < min_zoom) camera.zoom = min_zoom;
 
         if(IsKeyDown(KEY_UP) && grounded)
         {
@@ -73,6 +100,46 @@ int main ()
         {
         x_force += move_force;
         }
+        
+        //mouse camera manipulation
+        Vector2 init_mouse_pos;
+        if(!(IsCursorOnScreen() && IsMouseButtonDown(MOUSE_BUTTON_MIDDLE))) init_mouse_pos = GetMousePosition();
+        if(IsCursorOnScreen() && IsMouseButtonDown(MOUSE_BUTTON_MIDDLE))
+        {
+            Vector2 act_mouse_pos{(GetMousePosition())};
+            camera_position.x = act_mouse_pos.x - init_mouse_pos.x;
+            camera_position.y = act_mouse_pos.y - init_mouse_pos.y;
+            if (camera_position.x > screen_width*2) camera_position.x = screen_width*2;
+            if (camera_position.x < -screen_width) camera_position.x = -screen_width;
+            if (camera_position.y > screen_width*2) camera_position.y = screen_width*2;
+            if (camera_position.y < -screen_height) camera_position.y = -screen_height;
+        }
+        else init_mouse_pos = (Vector2){GetMousePosition()};
+
+
+
+
+        //camera manipulation
+        if(IsKeyDown(KEY_W))
+        {
+        camera_position.y -= 10;
+
+        }
+        if(IsKeyDown(KEY_S))
+        {
+        camera_position.y += 10;
+        }
+
+        if(IsKeyDown(KEY_A))
+        {
+        camera_position.x -= 10;
+        }
+
+        if(IsKeyDown(KEY_D))
+        {
+        camera_position.x += 10;
+        }
+
 
         //brake
         if(IsKeyDown(KEY_SPACE) && grounded)
@@ -82,12 +149,11 @@ int main ()
         
         if(IsKeyDown(KEY_X))
         {
-        circle_x = w_width/2;
-        circle_y = w_height/2;
+        circle_x = screen_width/2;
+        circle_y = screen_height/2;
         x_speed = 0;
         y_speed = 0;
         }
-        //calculating acceleration
         //calculating acceleration
 
         accl_x = x_force/circle_mass;
@@ -120,12 +186,7 @@ int main ()
 
         x_force = 0;
         y_force = 0;
-
-
-Rectangle rectangle1 {1,2,3,4};
-
-        //end of game logic
-
+        EndMode2D();
         EndDrawing();
     }
     std::cout << "DONE! See ya ";
