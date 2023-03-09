6e2b2e185aa396f14e05f02ff95660cee66f3552
Wolfgang_CZ <kralicek.roman@gmail.com>
2023-03-09 21:47:30 +0100

    working arrays

--- _code snippets/pointer array testing sorting/main.cpp
+++ _code snippets/pointer array testing sorting/main.cpp
@@ -38,13 +38,38 @@ class Test
         {
             m_y = a;
         }
+
+
     private:
         int m_x {std::rand()%100};
         int m_y {std::rand()%100};
 };
 
+    void sort_by_x(std::vector<std::shared_ptr<Test>> &tmp)
+    {
+        while(true)
+        {
+            std::size_t count {0};
+            count = 0;
+            for(std::size_t i{1}; i<tmp.size(); ++i)
+            {
+                if(tmp[i]->get_x() > tmp[i+1]->get_x())
+                std::swap(tmp[i],tmp[i+1]);
+                count++;
+            }
+            if(count == 0) break;
+        }
+    }
+
 struct TestArray
 {
+    void print()
+    {
+        for(std::size_t i {0}; i<class_arr.size(); ++i)
+        {
+            class_arr[i]->print();
+        }
+    }
     std::vector <std::shared_ptr<Test>> class_arr{};
 };
 
@@ -52,7 +77,7 @@ struct TestArray
 int main ()
 {
     TestArray test_array;
-    const std::size_t range {3};
+    const std::size_t range {10};
     std::srand(time(0));
 
     std::cout << "first filling" << std::endl;
@@ -71,10 +96,9 @@ int main ()
         std::cout << "pos: " << i << ", ";
         test_array.class_arr[i]->print();
     }
-    for(std::size_t i {0}; i<range; ++i)
-    {
-        test_array.class_arr.erase(test_array.class_arr.begin()+i);
-    }
+    sort_by_x(test_array.class_arr);
+    test_array.print();
+
     
 
     std::cout << "ending properly" << std::endl;
