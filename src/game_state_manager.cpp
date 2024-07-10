dd822759c969afdda266dff794cc0ad603040ab4
Wolgang <kralicek.roman@gmail.com>
2024-07-10 21:32:25 +0200

    still refactoring

--- src/game_state_manager.cpp
+++ src/game_state_manager.cpp
@@ -8,3 +8,12 @@ void GameStateManager::set_game_state(const GameState &state)
 {
     game_state = state;
 }
+
+std::shared_ptr<GameStateManager> GameStateManager::get_instance()
+{
+    if(instance == nullptr) 
+    {
+        instance = std::make_shared<GameStateManager> ();  
+    }
+    return instance;
+}
--- src/game_state_manager.h
+++ src/game_state_manager.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 enum class GameState
 {
     GAME_STATE_GLOBAL = 0,
@@ -11,10 +13,17 @@ enum class GameState
 
 class GameStateManager
 {
+    private:
+        static std::shared_ptr<GameStateManager> instance;
+        GameStateManager(){};
+        GameStateManager(const GameState &) = delete;
+        GameStateManager& operator = (const GameStateManager &) = delete;
+        GameState game_state = GameState::GAME_STATE_PLAYING;
+
     public:
         GameState get_game_state();
         void set_game_state(const GameState &state);
-
-    private:
-        GameState game_state = GameState::GAME_STATE_PLAYING;
+        static std::shared_ptr<GameStateManager> get_instance();
 };
+
+std::shared_ptr<GameStateManager> game_state_manager = std::make_shared<GameStateManager> ();
