0c3d36b8da8f0401fa0aafc6dab951d19b0e2162
Wolgang <kralicek.roman@gmail.com>
2024-11-21 23:03:20 +0100

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include "./include/raylib.h"
 
+struct Config
+{
+    int win_h = 600;
+    int win_v = 800;
+};
+
+class Game
+{
+
+};
+
 int main()
 {
     std::cout << "START" << std::endl;
