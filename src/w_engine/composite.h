317e3d45de19cdebd926254b3c00ed055c364ed4
Wolgang <kralicek.roman@gmail.com>
2024-12-26 22:46:39 +0100

    alsfs

--- src/w_engine/composite.h
+++ src/w_engine/composite.h
@@ -7,10 +7,23 @@ namespace wEng
     class WComposite
     {
         private:
-            const std::vector<WPoint *> &m_objects;
+            const std::vector<WPoint *> &m_points;
         public:
-            WComposite(const std::vector<WPoint *> &w_objects): m_objects(w_objects)
+            WComposite(const std::vector<WPoint *> &w_objects): m_points(w_objects)
             {}
+    };
 
+    class CompositFactory
+    {
+        public:
+            static WComposite make_circle(int num_of_points, float radius)
+            {
+                std::vector<WPoint *> points;
+                for (int i = 0; i < num_of_points; i++)
+                {
+                    WPoint *point = new WPoint({radius * cos(i * 2 * PI / num_of_points), radius * sin(i * 2 * PI / num_of_points)});
+                }
+                return WComposite(points);
+            }
     };
 }
--- src/w_engine/premade_composites/circle.h
+++
@@ -1,6 +0,0 @@
-#include <vector>
-#include "../w_object.h"
-#include "../circle.h"
-
-
-std::vector<WObject *> 
